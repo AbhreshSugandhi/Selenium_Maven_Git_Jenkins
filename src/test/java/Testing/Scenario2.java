e0a7c63f3eb17aaaf7b4c320d773da535a36f2c9
tabhresh <tabhresh@gmail.com>
2018-09-11 17:29:49 +0530

    4th Commit

--- src/test/java/Testing/Scenario2.java
+++ src/test/java/Testing/Scenario2.java
@@ -1,49 +1,31 @@
-package Testing;
+package module6;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Scenario2 {
+public class Sequencing {
 
-	WebDriver driver;
-
-	@Test
-	public void openApp() {
-
-		// Browser Instantiating Or Open Browser
-		System.setProperty("webdriver.chrome.driver", "D:\\Training\\Installation_stuff\\ExeFiles\\chromedriver.exe");
-		driver = new ChromeDriver();
-
-		// Open AUT
-		driver.get("http://newtours.demoaut.com/");
-		
-		System.out.println("AUT opened Succesfully");
+	@Test(priority = 0)
+	public void Sugandh() {
+		System.out.println("This is the Test Case 1");
 	}
 
-	@Test(dependsOnMethods = "openApp")
-	public void Login() throws InterruptedException {
-		// Enter UserID
-		driver.findElement(By.name("userName")).sendKeys("tutorial");
-
-		// Enter Password
-		driver.findElement(By.name("password")).sendKeys("tutorial");
-
-		// Click on Sign in button
-		driver.findElement(By.name("login")).click();
-		Thread.sleep(3000);
-		System.out.println("Logged In Successfully");
-
+	@Test(priority = 1, enabled = true)
+	public void Kanika() {
+		System.out.println("This is the Test Case 2");
 	}
 
-	@Test(dependsOnMethods = "Login")
-	public void Logout() throws InterruptedException {
-		// Click on Sign off
-		driver.findElement(By.linkText("SIGN-OFF")).click();
-		System.out.println("Logged Off successfully");
-		Thread.sleep(3000);
-		driver.quit();
+	@Test(priority = 2)
+	public void Thrishna() {
+		System.out.println("This is the Test Case 3");
 	}
 
+	@Test(priority = 3)
+	public void Tai() {
+		System.out.println("This is the Test Case 4");
+	}
+	
+	@Test(priority = 4)
+	public void Madhava() {
+		System.out.println("This is the Test Case 5");
+	}
 }
